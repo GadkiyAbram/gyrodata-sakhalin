91dd0ce678948b3490df914620b44a105f8c0de1
GadkiyAbram <alex-abram@bk.ru>
2020-08-07 09:29:58 +0700

    updated home screen after login. Adopted for mobile device

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -1,13 +1,18 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	<meta http-equiv="X-UA-Compatible" content="ie=edge"/>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-{{--    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>--}}
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+	
+	<link href="{{ asset('css/style.css') }}" rel="stylesheet">
+	<!--  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>  -->
 
 	<style>
+
 		.nav-link {
 			color: darkgrey;
 			font-family: SegoeUI-Regular-final, sans-serif;
@@ -28,69 +33,31 @@
 		      color: grey;
 		      font-family: SegoeUI-Regular-final, sans-serif;
 		      font-size: 18px;
-
 		}
 
 	</style>
-
 </head>
-
 <body>
-
-<div class="pb-2 pl-1 pr-1">
-
-	<div class="d-flex align-items-baseline justify-content-between shadow-sm">
-		<div>
-			<nav class="navbar navbar-expand-md navbar-light bg-white">
-				<div class="py-1">
-
-					<div class="collapse navbar-collapse" id="navbarSupportedContent">
-						<!-- Left Side Of Navbar -->
-						<ul class="navbar-nav">
-								<li class="nav-item">
-									<a class="nav-link" href="/">Main</a>
-								</li>
-								<li class="nav-item">
-									<a class="nav-link" href="/news">Updates</a>
-								</li>
-								<li class="nav-item">
-									<a class="nav-link" href="/tools">Tools</a>
-								</li>
-								<li class="nav-item">
-									<a class="nav-link" href="/jobs">Job Tracking</a>
-								</li>
-								<li class="nav-item">
-									<a class="nav-link" href="/batteries">Lithium Batteries</a>
-								</li>
-{{--								<li class="nav-item">--}}
-{{--									<a class="nav-link" href="/preferences">Preferences</a>--}}
-{{--								</li>--}}
-                                @can('isAdmin')
-                                <li class="nav-item">
-                                    <a class="nav-link" href="/master">Admin Panel</a>
-                                </li>
-                                @endcan
-							</ul>
-					</div>
-				</div>
-			</nav>
+	<nav class="d-flex">
+		<div class="navigation">
+			<div class="hamburger">
+				<div class="line"></div>
+				<div class="line"></div>
+				<div class="line"></div> 
+			</div>
+			<ul class="nav-links">
+				<li><a href="/">Main</a></li>
+				<li><a href="news">Updates</a></li>
+				<li><a href="tools">Tools</a></li>
+				<li><a href="jobs">Jobs</a></li>
+				<li><a href="batteries">Batteries</a></li>
+				@can('isAdmin')
+				<li><a href="master">Admin Panel</a></li>
+				@endcan
+			</ul>
 		</div>
-
-		<div class="d-flex align-items-baseline justify-content-between">
-			@if (Auth::user())
-{{--			<div class="dropdown mr-4">--}}
-{{--                <a data-toggle="dropdown" style="z-index: 9">{{ Auth::user()->name }}</a>--}}
-{{--                <div class="dropdown-menu">--}}
-{{--                	<a href="/preferences" class="dropdown-item-gdta">Settings</a>--}}
-{{--                	<a href="#" class="dropdown-item-gdta">Another</a>--}}
-{{--                    <a href="{{ route('logout') }}" class="dropdown-item-gdta" onclick="event.preventDefault();--}}
-{{--        			document.getElementById('logout-form').submit()">Sign out</a>--}}
-{{--                </div>--}}
-
-{{--                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">--}}
-{{--                    @csrf--}}
-{{--                </form>--}}
-{{--            </div>--}}
+		<div class="logo d-flex">
+		@if (Auth::user())
                 <div class="dropdown mr-4">
                     <a data-toggle="dropdown" style="z-index: 9" onclick="show_hide()">{{ Auth::user()->name }}</a>
                     <div class="dropdown-menu" id="drop-content" style="display: none;">
@@ -109,9 +76,8 @@
     			<img src="{{URL::asset('/gyrodata_logo_small.jpg')}}" height="50" width="200">
     		</div>
 		</div>
+	</nav>
 
-	</div>
-</div>
 <script type="text/javascript">
 
     function show_hide() {
@@ -126,7 +92,8 @@
         // window.onclick = function(event) {
         //     click.style.display = "none";
         // }
-    }
+	}
 </script>
+<script src={{URL::asset('js/navscript.js')}}></script>
 </body>
 </html>
